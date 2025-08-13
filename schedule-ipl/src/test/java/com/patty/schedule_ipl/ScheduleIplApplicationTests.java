da24beb5e7f9a5e50613e3b8c776b9e72feab8fb
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-13 23:49:37 +0530

    ScheduleApplication

---
+++ schedule-ipl/src/test/java/com/patty/schedule_ipl/ScheduleIplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patty.schedule_ipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleIplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
